14befb982243d691c240c7152dd790a7415c5b99
davidzheng3141 <77525517+davidzheng3141@users.noreply.github.com>
2021-07-13 21:36:21 -0700

    Create UniswapSpendLimit.sol

---
+++ UniswapSpendLimit.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+contract UniswapSpendLimit{
+    address constant USDCContract = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant UNIContract = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
+    address constant TimelockContract = 0x1a9C8182C09F50C8318d769245beA52c32BE35BC;
+    address public beneficiary;
+    
+    uint public UNISpendLimit; 
+    uint public USDCSpendLimit;
+    
+    modifier onlyUniGov{
+        require (msg.sender == TimelockContract);
+        _;
+    }
+    
+    modifier onlyBeneficiary{
+        require (msg.sender == beneficiary);
+        _;
+    }
+    
+    constructor(){
+        beneficiary = 0x1C95930Dfc1139381265ce45B5f480F1EFae09A1;
+    }
+    
+    function setBeneficiary(address newBeneficiary) public onlyBeneficiary{
+        beneficiary = newBeneficiary;
+    }
+    
+    // 0 = UNI, 1 = USDC
+    function withdrawTokens(uint token, uint amount) public onlyBeneficiary{
+        if (token == 1){
+            USDCSpendLimit -= amount;
+            IERC20(USDCContract).transfer(beneficiary, amount);
+        }
+        if (token == 0){
+            UNISpendLimit -= amount;
+            IERC20(UNIContract).transfer(beneficiary, amount);
+        }
+    }
+    
+    function recallFunds(address token, uint amount) public onlyUniGov{
+        IERC20(token).transfer(TimelockContract, amount);
+    }
+    
+    function increaseSpendLimit(uint uniLimit, uint usdcLimit) public onlyUniGov{
+        UNISpendLimit += uniLimit;
+        USDCSpendLimit += usdcLimit;
+    }
+    function setSpendLimit(uint uniLimit, uint usdcLimit) public onlyUniGov{
+        UNISpendLimit = uniLimit;
+        USDCSpendLimit = usdcLimit;
+    }
+}
